85bc57debfba522bb787a16020774e136e963d08
Pratyush Sharma <pratyush.sharma.from1999@gmail.com>
2019-04-24 23:30:31 +0530

    Update linearRegCostFunction.m

--- linearRegCostFunction.m
+++ linearRegCostFunction.m
@@ -8,16 +8,11 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
+% Linear regression cost function with regularisation and gradient descent (vectorised implementation) 
+
 h = X*theta ;
 diff = h - y ; 
 theta1 = [0 ; theta(2:end ,:), :] ;
